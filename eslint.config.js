8a9ff26fe68bbbaf156eff5de77ad6155cb723ba
Wojciech Choroś <wojciech.choros@dynamico.ai>
2025-01-29 22:43:38 +0100

    fix: prettier 2

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,31 @@
 // eslint.config.js
-import react from 'eslint-plugin-react';
-import ts from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react'
+import ts from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
 
 export default [
-  {
-    ignores: ['dist', 'node_modules'],
-  },
-  {
-    files: ['src/**/*.{ts,tsx}'],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true,
-        },
+   {
+      ignores: ['dist', 'node_modules'],
+   },
+   {
+      files: ['src/**/*.{ts,tsx}'],
+      languageOptions: {
+         parser: tsParser,
+         parserOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            ecmaFeatures: {
+               jsx: true,
+            },
+         },
       },
-    },
-    plugins: {
-      react,
-      '@typescript-eslint': ts,
-    },
-    rules: {
-      // your ESLint rules go here
-      'no-unused-vars': 'error',
-    },
-  },
-];
+      plugins: {
+         react,
+         '@typescript-eslint': ts,
+      },
+      rules: {
+         // your ESLint rules go here
+         'no-unused-vars': 'error',
+      },
+   },
+]
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
-import { defineConfig } from "vite";
-import { reactRouter } from "@react-router/dev/vite";
-import { configDefaults } from 'vitest/config';
+import { defineConfig } from 'vite'
+import { reactRouter } from '@react-router/dev/vite'
+import { configDefaults } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [
-    reactRouter(),
-  ],
-  test: {
-    ...configDefaults,
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: ['./__tests__/setupTests.ts'],
-  },
-});
+   plugins: [reactRouter()],
+   test: {
+      ...configDefaults,
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: ['./__tests__/setupTests.ts'],
+   },
+})
